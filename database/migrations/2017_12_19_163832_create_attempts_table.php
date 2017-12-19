8aff256b62832bec54a50fa19a086331aa5a7a57
Jordan Center <BigJman1234567@yahoo.com>
2017-12-19 11:58:54 -0600

    added more marvel characters

---
+++ database/migrations/2017_12_19_163832_create_attempts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttemptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attempts', function (Blueprint $table) {
+            $table->integer('attempts');
+            $table->boolean('times-correct');
+            $table->increments('correct-streak');
+            $table->dateTime('proficient-at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attempts');
+    }
+}
